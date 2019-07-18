189b45144c9ec81a5e688f8e7a56873fbcd59b27
xiaoheng666 <1919196455@qq.com>
2019-07-19 01:14:00 +0800

    改一下八哥

--- mylibrary/src/main/java/com/tmgp/conan/mylibrary/Dialog.java
+++ mylibrary/src/main/java/com/tmgp/conan/mylibrary/Dialog.java
@@ -6,36 +6,36 @@ import android.content.DialogInterface;
 
 public class Dialog
 {
-	public static void 对话框(Context 上下文, String 标题, String 内容, String 第一个内容, String 第二个内容, final 按钮点击事件监听 it)
+	public static void dialog(Context context, String title, String content, String oneButton, String twoButton, final ifOnclick it)
 	{
-		AlertDialog.Builder ad = new AlertDialog.Builder(上下文);
-		ad.setTitle(标题);
-		ad.setMessage(内容);
-		ad.setPositiveButton(第一个内容, new DialogInterface.OnClickListener()
+		AlertDialog.Builder ad = new AlertDialog.Builder(context);
+		ad.setTitle(title);
+		ad.setMessage(content);
+		ad.setPositiveButton(oneButton, new DialogInterface.OnClickListener()
 			{
 				@Override
 				public void onClick(DialogInterface p1, int p2)
 				{
 					// TODO: Implement this method
-					it.第一个按钮点击事件(p1, p2);
+					it.oneButtonClick(p1, p2);
 				}
 			});
-		ad.setNegativeButton(第二个内容, new DialogInterface.OnClickListener()
+		ad.setNegativeButton(twoButton, new DialogInterface.OnClickListener()
 			{
 				@Override
 				public void onClick(DialogInterface p1, int p2)
 				{
 					// TODO: Implement this method
-					it.第二个按钮点击事件(p1, p2);
+					it.twoButtonClick(p1, p2);
 				}
 			});
 		ad.show();
 	}
 
-	public interface 按钮点击事件监听
+	public interface ifOnclick
 	{
-		void 第一个按钮点击事件(DialogInterface DialogInterface对象, int 不知道这参数有啥用);
-		void 第二个按钮点击事件(DialogInterface DialogInterface对象, int 不知道这参数有啥用);
+		void oneButtonClick(DialogInterface DialogInterface对象, int 不知道这参数有啥用);
+		void twoButtonClick(DialogInterface DialogInterface对象, int 不知道这参数有啥用);
 	}
 
 }
